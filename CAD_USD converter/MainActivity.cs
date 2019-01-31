4a36793eb10a3a2a7353925c8ef68200eed87167
visualstudio <visualstudio@122-PC35>
2019-01-31 11:45:51 -0500

    calc

--- CAD_USD converter/MainActivity.cs
+++ CAD_USD converter/MainActivity.cs
@@ -12,17 +12,47 @@ namespace CAD_USD_converter
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-
+        TextView vcad, vusd;
+        EditText tcad, tusd;
+        Button btnConv, btnClr;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+            vcad = (TextView)FindViewById(Resource.Id.tvCAD);
+            vusd = (TextView)FindViewById(Resource.Id.tvUSD);
+            tcad = (EditText)FindViewById(Resource.Id.etCAD);
+            tusd = (EditText)FindViewById(Resource.Id.etUSD);
+            btnConv = (Button)FindViewById(Resource.Id.btnConvert);
+            btnClr = (Button)FindViewById(Resource.Id.btnClear);
+            btnConv.Click += delegate
+            {
+                if (tusd.Text != "")
+                {
+                    double ca, us;
+                    us = double.Parse(tusd.Text); //get the amount of money in cad from user to convert ti user
+                    ca = us * 1.33;
+                    tcad.Text = ca.ToString();
+                }
 
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+                else if (tcad.Text != "")
+                {
+                    double ca, us;
+                    ca = double.Parse(tcad.Text); //get the amount of money in cad from user to convert ti user
+                    us = ca / 1.33;
+                    tusd.Text = us.ToString();
+                }
+                else                
+                    Toast.MakeText(this, "Please enter amount to convert", ToastLength.Long).Show();               
+            };
+
+            btnClr.Click += delegate
+            {
+                tcad.Text = "";
+                tusd.Text = "";
+            };
+           
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
